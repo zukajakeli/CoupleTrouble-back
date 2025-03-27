66cca06b143ea2a788e98d04a1922350ccbe99e9
zuka.j <zuka.jayeli@gmail.com>
2025-03-27 23:44:35 +0400

    Fix: render  config

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as bodyParser from 'body-parser';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 
 async function bootstrap() {
-  const expressApp = express();
-  //For development
-  // const app = await NestFactory.create(AppModule);
-
-  //For production
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('My App API')
@@ -28,12 +18,11 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'localhost:3000' || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   await app.listen(process.env.PORT ?? 8000);
-
-  await app.init();
-
-  // Critical for Vercel deployment
-  return expressApp;
 }
-export default bootstrap();
+bootstrap();
