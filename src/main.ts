e678e4345ee84b20d9955d16f3def72aaa3934b1
zuka.j <zuka.jayeli@gmail.com>
2025-04-11 18:58:40 +0400

    Fi: for aws deployment

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env.PORT ?? 8000, '0.0.0.0');
 }
 bootstrap();
